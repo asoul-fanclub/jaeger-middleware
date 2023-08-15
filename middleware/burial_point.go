69120e2ca18091112ca9527f0676b75bf88646a5
Makonike <makonike@anyview.fun>
2023-08-15 23:38:02 +0800

    fix: remove SelectMethod

--- middleware/burial_point.go
+++ middleware/burial_point.go
@@ -1,8 +1,1 @@
 package middleware
-
-import "context"
-
-// SelectMethod
-// Firstly support Jaeger, and this will be customized by the caller
-// TODO: data
-type SelectMethod func(ctx context.Context, method string) error
